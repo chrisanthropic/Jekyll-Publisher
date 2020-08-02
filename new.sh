13e64836b932a87bf11ee45bc3a448054add715a
Blake Eryx <41544894+Blake-Eryx@users.noreply.github.com>
2020-08-02 19:47:56 -0400

    Update pandoc options, latex template, add new features (#17)
    * Update script to use pdf-engine, not latex-engine
    
    * Fix typos
    
    * Comment fixes
    
    * Fix misspellings
    
    * Make dedication section optional
    
    * Add (optional) TOC support
    
    * Fix typo
    
    * Add support for controlling the margins (PDF)
    
    * Add debug option to draw frames in PDF output
    
    * Mention toc option in skeleton project
    
    * Mention margin options in skeleton project
    
    * Add calligraphy font (not used, but could be useful)
    
    * Add support for using chapter header styles from fncychap
    
    * Add lettrine and adforn (used for making flourishes) support to print output
    
    * Add --top-level-division=chapter, pandoc had a change of behavior and this is needed to ensure H1 is seen as a chapter name
    
    * Fix typo, turn off calligraphy font for letterine
    
    * Add support for latex formatting title and subtitles
    
    * Update 'Linux Libertine' to Libertinus. This fixes smalls caps that weren't working, and adds better math symbol support.
    
    * Fix deprecated e-pub pandoc build flags
    
    * Use markdown+smart correctly
    
    * Add styles for small and drop caps in e-pub css
    
    * Update cs-6x9-pdf.latex
    
    Fix formatting issues.
    
    * Update cs-5x8-pdf.latex
    
    Fix formatting issues.

--- new.sh
+++ new.sh
@@ -80,6 +80,13 @@ epub-isbn:
 
 dedication: Dedicated to my readers.
 
+toc: false
+
+top-margin:
+bottom-margin:
+inner-margin:
+outer-margin:
+
 identifier:
     - scheme: UUID
       text: [Grab a free Version4 UUID from here: https://www.uuidgenerator.net/version4]
